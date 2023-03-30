a00d53b3f4e24884d8f0576ce324739eee4360e9
Ahmed Osama <aosama169@gmail.com>
2023-03-30 14:45:48 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * _strncpy - copy two strings
+ * @dest: pointer for destination
+ * @src: pointer for source string
+ * @n: number of charachters to copy
+ *
+ * Return: pointer of copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
