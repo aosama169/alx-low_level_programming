c8ef47b5b1d3ecc07cd77aa2c12304f9c5d4b830
Ahmed Osama <aosama169@gmail.com>
2023-03-30 15:04:13 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,14 @@
  *
  * Return: the resulting string
  */
+
 char *string_toupper(char *s)
 {
 	int i;
+
 	i = 0;
-	
-	while(s[i] != '\0')
+
+	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
