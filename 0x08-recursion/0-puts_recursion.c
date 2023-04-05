9159464a250c2483e53143b4b03f76468d545f09
Ahmed Osama <aosama169@gmail.com>
2023-04-05 13:36:29 -0700

    Put Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print string and new line
+ * @s: pointer to the string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
