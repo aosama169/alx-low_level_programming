5cc148c71de94c13f276de795ad81a7e96927c60
Ahmed Osama <aosama169@gmail.com>
2023-04-05 14:16:42 -0700

    wild

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings can be considered identical, otherwise return 0
+ * @s1: first string
+ * @s2: second string
+ * Return: number
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return 0;
+}
+
