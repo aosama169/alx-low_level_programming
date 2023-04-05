c2a4e82dc07633bd87213cba70db50e4907a3b73
Ahmed Osama <aosama169@gmail.com>
2023-04-05 13:44:33 -0700

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,13 @@
 /**
  * _strlen_recursion - return string length
  * @s: string
+ * Return: Number of Chars
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0'){
+	if (*s == '\0')
+	{
 		return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
