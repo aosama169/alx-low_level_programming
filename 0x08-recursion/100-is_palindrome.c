2f13ce50dc977eb07b99d8da88bb79ae6dbdf9c3
Ahmed Osama <aosama169@gmail.com>
2023-04-05 14:14:59 -0700

    plaindrom

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: string
+ * Return: integer
+ */
+
+int is_palindrome(char *s)
+{
+	return 0;
+}
