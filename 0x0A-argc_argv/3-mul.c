9f966583bcb0aa09fd96c126917072398b1c468d
Ahmed Osama <aosama169@gmail.com>
2023-04-09 17:09:59 -0700

    multiplier

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - num multiplier
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: success 0 , error 1
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
