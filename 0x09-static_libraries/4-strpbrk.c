fe722b9322c796aed0b15542fce3b7c54974dac5
Ahmed Osama <aosama169@gmail.com>
2023-04-09 17:44:32 -0700

    libme

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *s != '\0'; i++)
 	{
-		for (k = 0; accept[j] != '\0'; k++)
+		for (k = 0; accept[k] != '\0'; k++)
 		{
 			if (*s == accept[k])
 			{
