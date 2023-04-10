0037687796c8032acca498f422abf41daf335ca3
Ahmed Osama <aosama169@gmail.com>
2023-04-09 17:20:29 -0700

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coin to make change for an amount of money
+ * main - prints the minimum number of coin to make change
+ * for an amount of money
  * @argc: number of args
  * @argv: array of args
  *
