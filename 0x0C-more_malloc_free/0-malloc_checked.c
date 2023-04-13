c3d0071ee6b95eaa034ea0fb0ec5d2db624feb94
Ahmed Osama <aosama169@gmail.com>
2023-04-13 14:49:55 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	int *ptr1;
 
-	ptr = malloc(b);
+	ptr1 = malloc(b);
 
-	if (ptr == NULL)
+	if (ptr1 == NULL)
 		exit(98);
 
-	return (ptr);
+	return (ptr1);
 }
 
