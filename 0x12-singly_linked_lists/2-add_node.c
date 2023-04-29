2843ae65acd3dcf5afdb5dadafa6f3235e873baf
Ahmed Osama <aosama169@gmail.com>
2023-04-28 18:12:02 -0700

    commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - add node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int count = 0;
+
+	while (str[count])
+		count++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->count = count;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
