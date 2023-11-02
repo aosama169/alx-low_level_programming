af4cb97081600189d8d1dbc497c14563c0018203
Aosama <ahmed.osama@elsewedyintel.com>
2023-11-02 23:41:23 +0200

    fix array range
    Signed-off-by: Aosama <ahmed.osama@elsewedyintel.com>

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,14 +10,14 @@
 int *array_range(int min, int max)
 {
 	int *ptr1;
-	int i, length;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
 
-	length = max - min + 1;
+	size = max - min + 1;
 
-	ptr1 = malloc(lengthof(int) * length);
+	ptr1 = malloc(sizeof(int) * size);
 
 	if (ptr1 == NULL)
 		return (NULL);
