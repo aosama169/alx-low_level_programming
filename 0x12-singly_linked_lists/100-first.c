1274268fddd72430a4814b222dabb3c16397db33
Aosama <ahmed.osama@elsewedyintel.com>
2023-11-16 16:14:22 +0200

    fix escape char
    Signed-off-by: Aosama <ahmed.osama@elsewedyintel.com>

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@ void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-	printf("\(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.\)");
+	printf("\\(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.\\)");
 }
