14d05a7183e4f168739fb625e1a2a5b5a369dc24
Aosama <ahmed.osama@elsewedyintel.com>
2023-11-25 00:36:43 +0200

    binary to int
    Signed-off-by: Aosama <ahmed.osama@elsewedyintel.com>

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+
+/**
+ * binary_to_uint- convert binary to un signed int
+ * @b: binary
+ * Return: unsigned intger
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+	int night = 0, j;
+	unsigned int number = 0, non_binary = 0;
+
+	if (b == NULL)
+		return (non_binary);
+
+	while (b[night] != '\0')
+		night++;
+	night -= 1;
+	j = 0;
+	while (b[j])
+	{
+		if ((b[j] != '0') && (b[j] != '1'))
+			return (non_binary);
+
+		if (b[j] == '1')
+			number += (1 * (1 << night));
+		j++;
+
+		night--;
+	}
+	return (number);
+}
