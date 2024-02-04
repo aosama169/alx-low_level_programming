e42e851d2e3bc1caad6740ed39fc72d0cba5abf2
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-04 20:43:32 +0200

    has table set
    Signed-off-by: Aosama <ahmed.osama@elsewedyintel.com>

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - Add or update an element in a hash table
+ * @ht: Hash table to add or update key/value to
+ * @key: Key to add
+ * @value: Value of key
+ *
+ * Return: 1 if it succeeded
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *new_node, *current_node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	idx = key_idx((const unsigned char *)key, ht->size);
+	current_node = ht->array[idx];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
+}
