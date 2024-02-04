3d50bf5752094b7fbe2fd55cfcebc697bd636990
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-04 21:09:04 +0200

    format

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
